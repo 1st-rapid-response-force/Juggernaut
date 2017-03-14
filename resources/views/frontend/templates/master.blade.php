f512cc1284c064dff4c087747474be41d30cd753
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2017-03-14 01:05:24 -0600

    Inserting analytics

--- resources/views/frontend/templates/master.blade.php
+++ resources/views/frontend/templates/master.blade.php
@@ -66,6 +66,16 @@
 @include('frontend.includes.footer')
 @include('frontend.includes.sign-up')
 
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-35482408-7', 'auto');
+    ga('send', 'pageview');
+
+</script>
 
 <!-- Javascript -->
 @yield('before-scripts-end')
