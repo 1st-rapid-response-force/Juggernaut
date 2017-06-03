186e6f6496d870b3aafc9431cf3d6ee22f34f670
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2017-06-03 14:43:13 -0600

    Attempted fix

--- app/Console/Commands/CreateCAC.php
+++ app/Console/Commands/CreateCAC.php
@@ -90,8 +90,9 @@ class CreateCAC extends Command
                     'ximi.png',
                 ];
                 $images = public_path().'/img/faces/';
+                $faceDir = '/img/faces/';
                 $faces = '/img/faces/members/';
-                $face=\Image::make($images.$faces_array[$user->member->face_id])->resize(106,139);
+                $face=\Image::make($faceDir.$faces_array[$user->member->face_id])->resize(106,139);
                 // Create Image
                 $img = \Image::canvas(223,340);
                 $img->insert($images.'background.png');
