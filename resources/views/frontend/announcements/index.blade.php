8e041f95b6f915e22e029f00ba7fbcd7d6b4e93c
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2017-06-12 14:04:24 -0600

    Fixed formatting on announcement page

--- resources/views/frontend/announcements/index.blade.php
+++ resources/views/frontend/announcements/index.blade.php
@@ -1,6 +1,6 @@
 @extends('frontend.templates.master')
 
-@section('title','Our Mission')
+@section('title','Announcements')
 
 @section('content')
     <!-- wrapper -->
@@ -13,46 +13,40 @@
                 </ol>
             </div>
         </section>
-
         <section class="bg-grey-50">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-10 col-lg-offset-1">
                         @foreach($announcements as $announcement)
-                            <div class="panel panel-default panel-post">
-                                <div class="panel-body">
-                                    <div class="post">
-                                        <div class="post-header post-author">
-                                            <a href="{{route('frontend.files.file',$announcement->user->member->id)}}" class="author" data-toggle="tooltip" title="{{$announcement->user->member}}"><img src="{{$announcement->user->member->avatar}}" alt=""></a>
-                                            <div class="post-title">
-                                                <h3><a href="{{route('frontend.announcements.show',$announcement->id)}}">{{$announcement->subject}}</a></h3>
-                                                <ul class="post-meta">
-                                                    <li><a href="{{route('frontend.files.file',$announcement->user->member->id)}}"><i class="fa fa-user"></i> {{$announcement->user->member}}</a></li>
-                                                    <li><i class="fa fa-calendar-o"></i> {{$announcement->created_at->toFormattedDateString()}}</li>
-                                                </ul>
-                                            </div>
+                        <div class="panel panel-default panel-post">
+                            <div class="panel-body">
+                                <div class="post">
+                                    <div class="post-header post-author">
+                                        <a href="{{route('frontend.files.file',$announcement->user->member->id)}}" class="author" data-toggle="tooltip" title="{{$announcement->user->member}}"><img src="{{$announcement->user->member->avatar}}" alt=""></a>
+                                        <div class="post-title">
+                                            <h3><a href="{{route('frontend.announcements.show',$announcement->id)}}">{{$announcement->subject}}</a></h3>
+                                            <ul class="post-meta">
+                                                <li><a href="#"><i class="fa fa-user"></i> {{$announcement->user->member}}</a></li>
+                                                <li><i class="fa fa-calendar-o"></i> {{$announcement->created_at->toFormattedDateString()}}</li>
+                                            </ul>
                                         </div>
-                                        {!! str_limit($announcement->message,250) !!}
                                     </div>
-                                </div>
-                                <div class="panel-footer">
-                                    <ul class="post-action">
-                                        <li><a href="{{route('frontend.announcements.show',$announcement->id)}}"><i class="fa fa-book"></i> Read more</a></li>
-                                    </ul>
+                                    {!! str_limit($announcement->message,250) !!}
                                 </div>
                             </div>
-
+                            <div class="panel-footer">
+                                <ul class="post-action">
+                                    <li><a href="{{route('frontend.announcements.show',$announcement->id)}}"><i class="fa fa-book"></i> Read more</a></li>
+                                </ul>
+                            </div>
+                        </div>
                         @endforeach
-                    </div>
 
-                    <div class="text-center">
-                        {!! $announcements->links() !!}
+                        <div class="text-center">
+                            {!! $announcements->links() !!}
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
-
-
-
-
 @endsection
