e8bedfd7564dde03495be10ec3e15f1f6c594ca7
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2017-07-20 20:33:25 -0600

    Added TS to footer

--- resources/views/frontend/include/footer.blade.php
+++ resources/views/frontend/include/footer.blade.php
@@ -5,6 +5,7 @@
     <div class="footer-bottom">
         <div class="container">
             &copy; 2016 1st RRF. All rights reserved. <br>
+            Teamspeak: <a href="ts3server://ts.1st-rrf.com?port=9987&addbookmark=1st Rapid Response Force">ts.1st-rrf.com</a> <br>
             <img src="/img/logo_emblem.png">
             <br>
             <small>We are a gaming group for ARMA 3 - we are not affiliated or represent any real world entity<br>
