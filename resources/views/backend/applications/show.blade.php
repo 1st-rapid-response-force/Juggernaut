5fe774db21adca2ce05fe639fa323e73db08110e
Guillermo Rodriguez <guillermo@illuminationfoundry.com>
2017-08-23 00:00:24 -0600

    Modified app to be more in line with new direction, and added in more clear agreements to app

--- resources/views/backend/applications/show.blade.php
+++ resources/views/backend/applications/show.blade.php
@@ -176,6 +176,29 @@
                                     <label><input type="radio" name="agreement_ranks" value="0" {{$app->getApplication()->agreement_ranks == 0 ? 'checked' : ''}}> NO</label> &nbsp;
                                 </div>
                             </div>
+                            @if(isset($app->getApplication()->agreement_recruit))
+                            <div data-row-span="1">
+                                <div data-field-span="1">
+                                    <label>I UNDERSTAND I WILL BE EXPECTED TO SHOW UP TWICE A WEEK TO EVENTS (UNLESS I NOTIFY IN ADVANCE I WILL NOT BE ABLE TO ATTEND)</label>
+                                    <label><input type="radio" name="agreement_twice" value="1" {{$app->getApplication()->agreement_twice == 1 ? 'checked' : ''}}> YES</label> &nbsp;
+                                    <label><input type="radio" name="agreement_twice" value="0" {{$app->getApplication()->agreement_twice == 0 ? 'checked' : ''}}> NO</label> &nbsp;
+                                </div>
+                            </div>
+                            @endif
+                            @if(isset($app->getApplication()->agreement_recruit))
+                            <div data-row-span="2">
+                                <div data-field-span="1">
+                                    <label>I UNDERSTAND IF ACCEPTED INTO THE 1RRF, I WILL BE ASSIGNED RECRUIT STATUS AND CAN BE DISMISSED AT ANYTIME (UNTIL COMPLETION OF RECRUITMENT PHASE).</label>
+                                    <label><input type="radio" name="agreement_recruit" value="1" {{$app->getApplication()->agreement_recruit == 1 ? 'checked' : ''}}> YES</label> &nbsp;
+                                    <label><input type="radio" name="agreement_recruit" value="0" {{$app->getApplication()->agreement_recruit == 0 ? 'checked' : ''}}> NO</label> &nbsp;
+                                </div>
+                                <div data-field-span="1">
+                                    <label>I UNDERSTAND IF ACCEPTED INTO THE 1RRF, I WILL BE ASSIGNED TO AN EXISTING RRF MEMBER FOR TRAINING</label>
+                                    <label><input type="radio" name="agreement_buddy" value="1" {{$app->getApplication()->agreement_buddy == 1 ? 'checked' : ''}}> YES</label> &nbsp;
+                                    <label><input type="radio" name="agreement_buddy" value="0" {{$app->getApplication()->agreement_buddy == 0 ? 'checked' : ''}}> NO</label> &nbsp;
+                                </div>
+                            </div>
+                            @endif
                         </fieldset>
                         <br>
                         <div class="pull-right">
--- resources/views/frontend/apply/application.blade.php
+++ resources/views/frontend/apply/application.blade.php
@@ -138,21 +138,21 @@
                                                     <label><input type="radio" name="agreement_milsim" value="0" checked> NO</label> &nbsp;
                                                 </div>
                                                 <div data-field-span="1">
-                                                    <label>I UNDERSTAND THAT BY SUBMITTING THIS FORM, IT WILL IN EFFECT CHANGE MY STATUS AS A CIVILIAN TO A MEMBER OF THE 1ST RRF AND WILL BE HELD TO UNIT GUIDELINES</label>
-                                                    <label><input type="radio" name="agreement_guidelines" value="1"> YES</label> &nbsp;
-                                                    <label><input type="radio" name="agreement_guidelines" value="0" checked> NO</label> &nbsp;
+                                                    <label>I UNDERSTAND I WILL BE EXPECTED TO SHOW UP TWICE A WEEK TO EVENTS (UNLESS I NOTIFY IN ADVANCE I WILL NOT BE ABLE TO ATTEND)</label>
+                                                    <label><input type="radio" name="agreement_twice" value="1"> YES</label> &nbsp;
+                                                    <label><input type="radio" name="agreement_twice" value="0" checked> NO</label> &nbsp;
                                                 </div>
                                             </div>
                                             <div data-row-span="2">
                                                 <div data-field-span="1">
-                                                    <label>I UNDERSTAND THAT I AM EXPECTED TO FOLLOW ORDERS GIVEN TO ME</label>
-                                                    <label><input type="radio" name="agreement_orders" value="1"> YES</label> &nbsp;
-                                                    <label><input type="radio" name="agreement_orders" value="0" checked> NO</label> &nbsp;
+                                                    <label>I UNDERSTAND IF ACCEPTED INTO THE 1RRF, I WILL BE ASSIGNED RECRUIT STATUS AND CAN BE DISMISSED AT ANYTIME (UNTIL COMPLETION OF RECRUITMENT PHASE).</label>
+                                                    <label><input type="radio" name="agreement_recruit" value="1"> YES</label> &nbsp;
+                                                    <label><input type="radio" name="agreement_recruit" value="0" checked> NO</label> &nbsp;
                                                 </div>
                                                 <div data-field-span="1">
-                                                    <label>I UNDERSTAND THAT I AM EXPECTED TO RESPECT RANKS, CUSTOMS AND COURTESIES</label>
-                                                    <label><input type="radio" name="agreement_ranks" value="1"> YES</label> &nbsp;
-                                                    <label><input type="radio" name="agreement_ranks" value="0" checked> NO</label> &nbsp;
+                                                    <label>I UNDERSTAND IF ACCEPTED INTO THE 1RRF, I WILL BE ASSIGNED TO AN EXISTING RRF MEMBER FOR TRAINING</label>
+                                                    <label><input type="radio" name="agreement_buddy" value="1"> YES</label> &nbsp;
+                                                    <label><input type="radio" name="agreement_buddy" value="0" checked> NO</label> &nbsp;
                                                 </div>
                                             </div>
                                         </fieldset>
