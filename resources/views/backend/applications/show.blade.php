1c2dbe7af2264a236effafaf0d19067872329f66
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2017-08-29 11:04:03 -0600

    Removed duplicate

--- resources/views/backend/applications/show.blade.php
+++ resources/views/backend/applications/show.blade.php
@@ -179,58 +179,6 @@
                             </div>
                             @endif
                         </fieldset>
-                        <fieldset>
-                            <legend>AGREEMENTS</legend>
-                            <div data-row-span="2">
-                                <div data-field-span="1">
-                                    <label>I UNDERSTAND THAT I AM JOINING A MILITARY SIMULATION UNIT</label>
-                                    <label><input type="radio" name="agreement_milsim" value="1" {{$app->getApplication()->agreement_milsim == 1 ? 'checked' : ''}}> YES</label> &nbsp;
-                                    <label><input type="radio" name="agreement_milsim" value="0" {{$app->getApplication()->agreement_milsim == 0 ? 'checked' : ''}}> NO</label> &nbsp;
-                                </div>
-                                @if(isset($app->getApplication()->agreement_guidelines))
-                                <div data-field-span="1">
-                                    <label>I UNDERSTAND THAT BY SUBMITTING THIS FORM, IT WILL IN EFFECT CHANGE MY STATUS AS A CIVILIAN TO A MEMBER OF THE 1ST RRF AND WILL BE HELD TO UNIT GUIDELINES</label>
-                                    <label><input type="radio" name="agreement_guidelines" value="1" {{$app->getApplication()->agreement_guidelines == 1 ? 'checked' : ''}}> YES</label> &nbsp;
-                                    <label><input type="radio" name="agreement_guidelines" value="0" {{$app->getApplication()->agreement_guidelines == 0 ? 'checked' : ''}}> NO</label> &nbsp;
-                                </div>
-                                @endif
-                            </div>
-                            <div data-row-span="2">
-                                <div data-field-span="1">
-                                    <label>I UNDERSTAND THAT I AM EXPECTED TO FOLLOW ORDERS GIVEN TO ME</label>
-                                    <label><input type="radio" name="agreement_orders" value="1" {{$app->getApplication()->agreement_orders == 1 ? 'checked' : ''}}> YES</label> &nbsp;
-                                    <label><input type="radio" name="agreement_orders" value="0" {{$app->getApplication()->agreement_orders == 0 ? 'checked' : ''}}> NO</label> &nbsp;
-                                </div>
-                                <div data-field-span="1">
-                                    <label>I UNDERSTAND THAT I AM EXPECTED TO RESPECT RANKS, CUSTOMS AND COURTESIES</label>
-                                    <label><input type="radio" name="agreement_ranks" value="1" {{$app->getApplication()->agreement_ranks == 1 ? 'checked' : ''}}> YES</label> &nbsp;
-                                    <label><input type="radio" name="agreement_ranks" value="0" {{$app->getApplication()->agreement_ranks == 0 ? 'checked' : ''}}> NO</label> &nbsp;
-                                </div>
-                            </div>
-                            @if(isset($app->getApplication()->agreement_recruit))
-                            <div data-row-span="1">
-                                <div data-field-span="1">
-                                    <label>I UNDERSTAND I WILL BE EXPECTED TO SHOW UP TWICE A WEEK TO EVENTS (UNLESS I NOTIFY IN ADVANCE I WILL NOT BE ABLE TO ATTEND)</label>
-                                    <label><input type="radio" name="agreement_twice" value="1" {{$app->getApplication()->agreement_twice == 1 ? 'checked' : ''}}> YES</label> &nbsp;
-                                    <label><input type="radio" name="agreement_twice" value="0" {{$app->getApplication()->agreement_twice == 0 ? 'checked' : ''}}> NO</label> &nbsp;
-                                </div>
-                            </div>
-                            @endif
-                            @if(isset($app->getApplication()->agreement_recruit))
-                            <div data-row-span="2">
-                                <div data-field-span="1">
-                                    <label>I UNDERSTAND IF ACCEPTED INTO THE 1RRF, I WILL BE ASSIGNED RECRUIT STATUS AND CAN BE DISMISSED AT ANYTIME (UNTIL COMPLETION OF RECRUITMENT PHASE).</label>
-                                    <label><input type="radio" name="agreement_recruit" value="1" {{$app->getApplication()->agreement_recruit == 1 ? 'checked' : ''}}> YES</label> &nbsp;
-                                    <label><input type="radio" name="agreement_recruit" value="0" {{$app->getApplication()->agreement_recruit == 0 ? 'checked' : ''}}> NO</label> &nbsp;
-                                </div>
-                                <div data-field-span="1">
-                                    <label>I UNDERSTAND IF ACCEPTED INTO THE 1RRF, I WILL BE ASSIGNED TO AN EXISTING RRF MEMBER FOR TRAINING</label>
-                                    <label><input type="radio" name="agreement_buddy" value="1" {{$app->getApplication()->agreement_buddy == 1 ? 'checked' : ''}}> YES</label> &nbsp;
-                                    <label><input type="radio" name="agreement_buddy" value="0" {{$app->getApplication()->agreement_buddy == 0 ? 'checked' : ''}}> NO</label> &nbsp;
-                                </div>
-                            </div>
-                            @endif
-                        </fieldset>
                         <br>
                         <div class="pull-right">
                             @if($app->status == 1)
